9e8c9168ae94a1a6c8ec485567220da6d1c3763e
Alina <asalisinasa@gmail.com>
2016-02-17 22:21:26 +0300

    minor improvements

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 var textarea = document.querySelectorAll(".textarea");
 
 for (var i = 0; i < textarea.length; i++) {
-  textarea[i].addEventListener("focusout", function(event) {
-    var elem = this;
-    var placeholder = this.nextElementSibling;
-    var form_hint = placeholder.firstElementChild;
-    if(elem.value.length == 0) {
-      form_hint.classList.remove("lostfocus");
-    } else {
-      form_hint.classList.add("lostfocus");
-    }
-  }, false);
+    textarea[i].addEventListener("focusout", function(event) {
+        var elem = this;
+        var placeholder = this.nextElementSibling;
+        var form_hint = placeholder.firstElementChild;
+        if (elem.value.length == 0) {
+            form_hint.classList.remove("lostfocus");
+        } else {
+            form_hint.classList.add("lostfocus");
+        }
+    }, false);
 }
 
 
@@ -24,17 +24,17 @@ var link = document.querySelector(".adress-block__btn");
 var popup = document.querySelector(".feedback-form");
 var close = document.querySelector(".feedback-form__btn--close");
 
-if (link != null && popup != null) { 
+if (link != null && popup != null) {
     var name = popup.querySelector("[name=name]");
     var email = popup.querySelector("[name=email]");
     var text = popup.querySelector("[name=text]");
     var form = popup.querySelector("form");
 
-	link.addEventListener("click", function(event) {
-	   event.preventDefault();
-	   popup.classList.add("feedback-form--show");
-	   popup.classList.remove("feedback-form--error");
-	   name.focus()
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.add("feedback-form--show");
+        popup.classList.remove("feedback-form--error");
+        name.focus()
     });
     window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
@@ -53,15 +53,15 @@ if (link != null && popup != null) {
         }
     })
     close.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.remove("feedback-form--show");
-   });
+        event.preventDefault();
+        popup.classList.remove("feedback-form--show");
+    });
     cancel.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.remove("feedback-form--show");
-   });
+        event.preventDefault();
+        popup.classList.remove("feedback-form--show");
+    });
     mOverlay.addEventListener("click", function(e) {
-      if (e.tsrget == modal.parentNode)
-      popup.classList.remove("feedback-form--show");
-   });
+        if (e.tsrget == modal.parentNode)
+            popup.classList.remove("feedback-form--show");
+    });
 };
